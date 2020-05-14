75dca744b6b365f96e417fe1d33228a9f4983095
Dmitry Gorodnichy <dgo@jetthoughts.com>
2020-05-14 18:30:21 +0300

    Refactored sql query

--- db/views/film_ratings_v01.sql
+++ db/views/film_ratings_v01.sql
@@ -3,6 +3,6 @@ AVG(rate) AS rate,
 Films.id AS film_id,
 Films.title AS title,
 Films.genre AS genre
-FROM Reviews
-RIGHT OUTER JOIN Films ON Reviews.film_id=Films.id
+FROM Films
+RIGHT OUTER JOIN Reviews ON Reviews.film_id=Films.id
 GROUP BY Films.id;
